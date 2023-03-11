3f26ef465bf473389f9b9cce17bdbf06100870e7
MaxChuloshnikov <Maks447@ukr.net>
2023-03-11 16:40:31 +0200

    World Done Insides Start

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,5 +1,13 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { InsightCard, TitleText, TypingText } from '../components'
+
+import styles from '../styles';
+import { staggerContainer } from '../utils/motion';
+
+import { newFeatures } from '../constants';
+
 const Insights = () => (
   <section>
     Insights section
